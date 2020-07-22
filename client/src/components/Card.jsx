c993cb2bcb35b58481c258940dcf587af7841672
LuisDanilo <luis.danilo.95@hotmail.com>
2020-07-22 17:46:48 -0500

    [CLIENT] Tarjetas pullidas, fue necesario desechar un contendor extra usado en cards.jsx y adaptar card.jsx para que fuese su propio contenedor, asi se facilitaba la asignacion de las clases css que dan los efectos logrados hasta ahora, falta arreglar la animación cuando las tarjetas deben volver a mostrarse luego de girar una tarjeta

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -10,29 +10,28 @@ class Card extends Component {
     }
   }
 
-  showCard = () => {
+  toggleCard = () => {
     if (this.state.front) {
       this.card.classList.remove('flipToFront')
-      this.card.classList.add('flipToBack')
+      this.card.classList.add('flipToBack', 'grow')
       this.setState({front: false})
+      this.props.onFlipToFront()
     } else {
-      this.card.classList.remove('flipToBack')
+      this.card.classList.remove('flipToBack', 'grow')
       this.card.classList.add('flipToFront')
       this.setState({front: true})
+      this.props.onFlipToBack()
     }
-    
   }
 
   render() {
     return (
-      <div className="card-container" ref={element => this.card = element} onClick={this.showCard}>
+      <div className="my-3 card-container" onClick={this.toggleCard} ref={element => this.card = element}>
         <div className="card front">
-          <img src={image} alt="A misterious card"/>
+          <img className='' src={image} alt="A misterious card"/>
         </div>
-        <div className="card back">
-          
-        </div>
-      </div>
+        <div className="card back"></div>
+      </div> 
     )
   }
 }
--- client/src/components/Cards.jsx
+++ client/src/components/Cards.jsx
@@ -1,18 +1,36 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import Card from './Card'
 //import Score from './Score'
 
-const CardsSm = () => {
-  return (
-    <div className="row">
-      <div className="col-6 my-3"><Card/></div>
-      <div className="col-6 my-3"><Card/></div>
-      <div className="col-6 my-3"><Card/></div>
-      <div className="col-6 my-3"><Card/></div>
-    </div>
-  )
-}
+class CardsSm extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      expanded: false
+    }
+  }
 
+  expandCard = () => {
+    this.setState({expanded: true})
+    this.row.classList.add('expandChild')
+  }
+
+  shrinkCard = () => {
+    this.setState({expanded: false})
+    this.row.classList.remove('expandChild')
+  }
+
+  render() {
+    return (
+      <div className="row" ref={element => this.row = element}>
+        <Card onFlipToFront={this.expandCard} onFlipToBack={this.shrinkCard}/>
+        <Card onFlipToFront={this.expandCard} onFlipToBack={this.shrinkCard}/>
+        <Card onFlipToFront={this.expandCard} onFlipToBack={this.shrinkCard}/>
+        <Card onFlipToFront={this.expandCard} onFlipToBack={this.shrinkCard}/>
+      </div>
+    )
+  }
+}
 class Cards extends Component {
 
   constructor(props) {
@@ -27,11 +45,11 @@ class Cards extends Component {
   }
   resize = () => {
     let w = window.innerWidth
-    if (0 <= w && w <= 767) this.setState({sm: true, md: false, lg: false, xl: false})
-    else if (768 <= w && w <= 991) this.setState({sm: false, md: true, lg: false, xl: false})
-    else if (992 <= w && w <= 1199) this.setState({sm: false, md: false, lg: true, xl: false})
-    else if (w >= 1200) this.setState({sm: false, md: false, lg: false, xl: true})
-    else this.setState({sm: true, md: false, lg: false, xl: false})
+    if (0 <= w && w <= 767) this.setState({ sm: true, md: false, lg: false, xl: false })
+    else if (768 <= w && w <= 991) this.setState({ sm: false, md: true, lg: false, xl: false })
+    else if (992 <= w && w <= 1199) this.setState({ sm: false, md: false, lg: true, xl: false })
+    else if (w >= 1200) this.setState({ sm: false, md: false, lg: false, xl: true })
+    else this.setState({ sm: true, md: false, lg: false, xl: false })
     console.log(this.state)
   }
 
@@ -44,28 +62,12 @@ class Cards extends Component {
   }
 
   render() {
-    // Temporal components
-    let cardsMd = 
-      <div className="row">
-        <h1>MD</h1>
-      </div>
-    let cardsLg = 
-      <div className="row">
-        <h1>LG</h1>
-      </div>
-    let cardsXl = 
-      <div className="row">
-        <h1>XL</h1>
-      </div>
     return (
       <div className="container d-flex justify-content-center flex-column align-content-center cards-container">
         {/* <Score/> */}
         {/* Para pantallas por debajo de sm se muestran dos tarjetas por columna */}
         {/* A partir de pantallas md se desea mostrar tres tarjetas por columna */}
-        { this.state.sm ? <CardsSm/> : 
-          this.state.md ? cardsMd :
-          this.state.lg ? cardsLg :
-          this.state.xl ? cardsXl : <CardsSm/> }
+        {this.state.sm ? <CardsSm /> : <CardsSm />}
       </div>
     )
   }
