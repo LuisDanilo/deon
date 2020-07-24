64124ca9612514d48fe778fedfbbd5c50fb621d8
LuisDanilo <luis.danilo.95@hotmail.com>
2020-07-24 00:21:21 -0500

    [CLIENT] Se logró crear las targetas necesarias según la pantalla, renderizarlas dentro de un contenedro con css grid, establecer el numero de filas y columnas de dicho contenedor segun la pantalla y simular cuando una tarjeta es respondida (aumentando el contador de tarjetas respondidas y en caso de haberse repondido todas no renderizar mas tarjetas)

--- client/src/components/Cards/Cards.jsx
+++ client/src/components/Cards/Cards.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import Card from '../Card'
 //import Score from './Score'
+import cssVars from '../../assets/scss/vars.scss';
 
 class CardsSm extends Component {
   constructor(props) {
@@ -40,20 +41,38 @@ class Cards extends Component {
       md: false,
       lg: false,
       xl: false,
-      answered: 0
+      answered: 0,
+      repeatCards: 3,
+      gridCols: 2,
+      gridRows: 2
     }
-    this.cards = [
-      <Card/>, <Card/>, <Card/>, <Card/>, <Card/>, <Card/>,
-      <Card/>, <Card/>, <Card/>, <Card/>, <Card/>, <Card/>
-    ]
+    this.nCards = Number(cssVars.nCards)
   }
   resize = () => {
+    // Obtén ancho viewport
     let w = window.innerWidth
-    if (0 <= w && w <= 767) this.setState({ sm: true, md: false, lg: false, xl: false })
-    else if (768 <= w && w <= 991) this.setState({ sm: false, md: true, lg: false, xl: false })
-    else if (992 <= w && w <= 1199) this.setState({ sm: false, md: false, lg: true, xl: false })
-    else if (w >= 1200) this.setState({ sm: false, md: false, lg: false, xl: true })
-    else this.setState({ sm: true, md: false, lg: false, xl: false })
+    // Define qué tipo de pantalla es
+    let sm = (0 <= w && w <= 767)
+    let md = (768 <= w && w <= 991)
+    let lg = (992 <= w && w <= 1199)
+    let xl = (w >= 1200)
+    // Define según la pantalla cuantas cartas ha de presentar
+    let gridRows = sm || md ? cssVars.gridRowsSmMd :
+                   lg || xl ? cssVars.gridRowsLgXl : cssVars.gridRowsSmMd
+    let gridCols = sm ? cssVars.gridColsSm :
+                   md ? cssVars.gridColsMd :
+                   lg || xl ? cssVars.gridColsLgXl : cssVars.gridColsSm
+    // Define según la pantalla cuantas veces se repite un set de cartas
+    let repeatCards = this.nCards / (gridCols * gridRows)
+    // Añade la clase respectiva al contenedor de cartas
+    let grid = sm ? 'grid-sm' :
+               md ? 'grid-md' :
+               lg ? 'grid-lg' :
+               xl ? 'grid-xl' : 'grid-sm'
+    this.cardsContainer.classList.remove('grid-sm', 'grid-md', 'grid-lg', 'grid-xl')
+    this.cardsContainer.classList.add(grid)
+    // Actualiza estado
+    this.setState({sm, md, lg, xl, repeatCards, gridRows, gridCols})
   }
 
   componentDidMount = () => {
@@ -65,12 +84,37 @@ class Cards extends Component {
     window.removeEventListener("resize", this.resize)
   }
 
+  cardAnswered = (e) => {
+    let answered = this.state.answered + 1
+    e.target.classList.add('disabled')
+    this.setState({answered})
+  }
+
   render() {
+/*     let {sm, md, lg, xl, repeatCards, gridRows, gridCols} = this.state
+    let message = sm ? `Soy sm, (${gridRows}x${gridCols})x${repeatCards}` :
+                  md ? `Soy md, (${gridRows}x${gridCols})x${repeatCards}` :
+                  lg ? `Soy lg, (${gridRows}x${gridCols})x${repeatCards}` :
+                  xl ? `Soy xl, (${gridRows}x${gridCols})x${repeatCards}` :
+                  `Soy default, repito: ${repeatCards}` */
+                  
+    let {gridCols, gridRows, answered} = this.state
+    // Renderizar tarjetas si aun hay taretas por responder
+    let renderCards = (answered !== this.nCards)
+    console.log(`Respondidas: ${answered}, Total: ${this.nCards}, render?: ${renderCards}`)
+    // Obtener tarjetas
+    let cards = []
+    for (let i = 0; i < gridCols * gridRows; i++) {
+      cards.push(<button key={i} className='btn btn-info' onClick={this.cardAnswered}>Responder tarjeta</button>)
+    }
     return (
-      this.state.sm ? <h1>Soy sm</h1> :
-      this.state.md ? <h1>Soy md</h1> :
-      this.state.lg ? <h1>Soy lg</h1> :
-      this.state.xl ? <h1>Soy xl</h1> : <h1>Soy default</h1>
+      <div className="score-cards-container">
+        <h1>Score = {this.state.answered}</h1>
+        <div className="cards-container" ref={element => this.cardsContainer = element}>
+          {renderCards ? cards.map(card => card) : <h2>Ya es toda we</h2> }
+        </div>
+      </div>
+      
     )
   }
 }
